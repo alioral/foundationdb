b48d0350d2c525baccee50e41d4ccb79b25cc9a0
Ali Oral <alioral@windowslive.com>
2014-08-18 03:01:24 +0300

    minor changes

--- foundation/settings.py
+++ foundation/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4o_f880^n#g3*)ry2fo1ml)y(c93%g@-35&9obl_wu8!kga8$d'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
